18689796f7d520f8489d43a81ea3ee6197a7f877
rpunet <r.punetcorvillo@gmail.com>
2021-02-27 15:29:57 +0100

    echoypwdpiped

---
+++ minishell_v1/builtins.c
@@ -0,0 +1,140 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpunet <rpunet@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/20 19:54:34 by rpunet            #+#    #+#             */
+/*   Updated: 2021/02/27 15:27:44 by rpunet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int		doublelen(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while(arr[i])
+		i++;
+	return i;
+}
+
+void	ft_echo2(t_job *job, int j)
+{
+	int i;
+	char **args = job->cmds[j].args;
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (job->cmds[j].io[WRITE] != -1)
+			dup2(job->cmds[j].io[WRITE], STDOUT_FILENO);
+		ft_close_fds(job);
+		if (!args[1])
+		{
+			write(1, "\n", 1);
+			exit(0);
+		}
+		i = ft_strcmp(args[1], "-n") ? 1 : 2;
+		//ft_printf("builtIN-%s: ", args[0]);
+		while (args[i + 1])
+		{
+			ft_printf("%s ", args[i]);
+			i++;
+		}
+		ft_printf("%s", args[i]);
+		if (ft_strcmp(args[1], "-n"))
+			write(1, "\n", 1);
+		exit(0);
+	}
+}
+
+/* int		ft_pwd(char **args)
+{
+	char 	*ret;
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (doublelen(args) == 1)
+		{
+			ret = getcwd(NULL, 0);
+			//ft_printf("builtIN-%s: ", args[0]);
+			ft_putstr_fd(ret, STDIN_FILENO);
+			free (ret);
+		}
+		else
+		{
+			ft_printf("pwd: too many arguments\n");
+		}
+	}
+	return 0;
+}
+ */
+void	ft_pwd2(t_job *job, int j)
+{
+	char 	*ret;
+	pid_t	pid;
+
+	//ft_printf("builtedPWD");
+	pid = fork();
+	if (pid == 0)
+	{
+		if (job->cmds[j].io[WRITE] != -1)
+			dup2(job->cmds[j].io[WRITE], STDOUT_FILENO);
+		ft_close_fds(job);
+		if (doublelen(job->cmds[j].args) == 1)
+		{
+			ret = getcwd(NULL, 0);
+			ft_printf("builtIN-%s\n",ret);
+			//ft_putstr_fd(ret, STDIN_FILENO);
+			free (ret);
+			exit(0);
+		}
+		else
+		{
+			ft_printf("pwd: too many arguments\n");
+			exit(0);
+		}
+	}
+/* 	else if (pid < 0)
+		gestion de error */
+	// else
+	// 	while (waitpid(pid, NULL, 0) > 0);
+}
+
+
+
+
+
+
+
+
+
+int	ft_cd(char **args)			// esto esta fallandoooo --------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-----------------------
+{
+	//ft_printf("LLGA A DIR");
+	char *ret;
+	//ret = getcwd(NULL, 0);
+	ret = args[1];
+	if (chdir(ret) == -1)
+		ft_printf("cd error");
+	//perror("cd:");
+	return 0;
+}
+
+int		ft_exit(char **args)
+{
+	//ft_printf("builtIN-%s: ", args[0]);
+	//if (pid != 0)
+	if (args[0])
+		exit(EXIT_SUCCESS);
+	return 0;
+}
+
+
---
+++ minishell_v1/minishell.h
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpunet <rpunet@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/20 19:51:50 by rpunet            #+#    #+#             */
+/*   Updated: 2021/02/27 15:25:18 by rpunet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include "../../libftPLUS/includes/libft.h"
+
+# define READ		0
+# define WRITE		1
+# define BUILTINS	2 // 4
+
+// STRUCTS   --------------------------------
+typedef struct	s_cmd
+{
+	char	*name;
+	int		io[2];
+	char	**args;
+}				t_cmd;
+
+typedef struct	s_job
+{
+	int		n_cmds;
+	int		n_pipes;
+	int		(*fds)[2];
+	t_cmd	*cmds;
+}				t_job;
+
+// BUILTINS ------------------------------
+int		doublelen(char **arr);
+int		ft_echo(char **args);
+void	ft_echo2(t_job *job, int j);
+int		ft_pwd(char **args);
+void	ft_pwd2(t_job *job, int j);		// uniformar argumentos de todas las (*func)
+int		ft_cd(char **args);
+int		ft_exit(char **args);
+
+// extern char	*builtins[];
+// extern void	(*ft_builtins[])(char **);
+
+void	ft_close_fds(t_job *job);
+int		ft_execute(t_job *job);
+void	ft_waitfor(int n);
+t_job	*ft_parse(char *line);
+int		ft_get_input(char **line);
+void	ft_piping(t_job *job);
+int		main(int argc, char **argv);
+
+#endif
