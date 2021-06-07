c280dd910e34945e56bb7f9855fd04f701ca0b13
jjcarretero <jjcarretero@hotmail.com>
2021-06-08 00:12:00 +0200

    Updated Exit

--- minishell/exit.c
+++ minishell/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpunet <rpunet@student.42madrid.com>       +#+  +:+       +#+        */
+/*   By: jcarrete <jcarrete@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 21:54:49 by jcarrete          #+#    #+#             */
-/*   Updated: 2021/06/01 22:59:55 by rpunet           ###   ########.fr       */
+/*   Updated: 2021/06/08 00:11:35 by jcarrete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,57 @@ void	free_char_array(char **arr, int size)
 	arr = ft_memfree(arr, NULL);
 }
 
-int	exit_failure(char *format, ...)
-{
-	ft_dprintf(STDERR_FILENO, "ERROR:\n");
-	ft_dprintf(STDERR_FILENO, format);
-	exit(EXIT_FAILURE);
-	return (EXIT_SUCCESS);
-}
-
 void	*return_null(char *ptr)
 {
 	ptr = ft_memfree(ptr, NULL);
 	return (NULL);
+}
+
+static void	failure_puttype(char *format, int *i, va_list *ap)
+{
+	char	*s1;
+
+	if (format[*i] == 's')
+	{
+		s1 = va_arg(*ap, char *);
+		if (s1 == NULL)
+			ft_dprintf(STDERR_FILENO, "(null)");
+		else
+			ft_dprintf(STDERR_FILENO, s1);
+		(*i)++;
+	}
+	else if (format[*i] == 'c')
+	{
+		ft_putchar_fd((char)va_arg(*ap, int), STDERR_FILENO);
+		(*i)++;
+	}
+	else if (format[*i] == 'i')
+	{
+		s1 = ft_itoa((int)(va_arg(*ap, int)));
+		ft_dprintf(STDERR_FILENO, s1);
+		(*i)++;
+	}
+}
+
+int	exit_failure(char *format, ...)
+{
+	va_list	ap;
+	int		i;
+
+	va_start(ap, format);
+	ft_dprintf(STDERR_FILENO, "ERROR:\n");
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			failure_puttype(format, &i, &ap);
+		}
+		else
+			ft_putchar_fd(format[i++], STDERR_FILENO);
+	}
+	va_end(ap);
+	exit(EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
